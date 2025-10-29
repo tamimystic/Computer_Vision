7f5306aa1c0d89a3846f0f5e0239774f417f9c55
tamimystic <tamimhossainforcoding01@gmail.com>
2025-10-30 04:06:26 +0600

    convert blur image to canny image in basic function code in day2 folder

--- day2/basic_function.py
+++ day2/basic_function.py
@@ -21,5 +21,13 @@ img_blur=cv2.GaussianBlur(img_gray,(7,7),0) #kernel/filter size and 0 means dens
 print(img_blur)
 print(img_blur.shape)
 blur_img_dekha=cv2.imshow("blur image", img_blur)
-cv2.waitKey(0)
 #we convert to gray scale beacause reduce our complexity,beacause to convert blur we just want to know the edges
+
+
+print("*********************edge cascade(canny) image***********************")
+#Convert to edge cascade image 
+img_canny=cv2.Canny(img_blur,100,100) #100,100 mane threshold value, that means jodi kono pixel er value 100 er besi hoy tahole seta edge hisebe consider kora hobe
+print(img_canny)
+print(img_canny.shape)
+canny_img_dekha=cv2.imshow("canny image", img_canny)
+cv2.waitKey(0)
